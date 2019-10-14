34ba5111e627879aaa90ce96fa59186f3882f002
Jose Antonio <llytocolegio123@gmail.com>
2019-10-14 09:12:18 +0200

    Creacion tabla

---
+++ create.sql
@@ -0,0 +1,7 @@
+create table usuario (
+    id            int Primary key not null,
+    nombre        varchar(30) not null, 
+    Apellidos     varchar(30) not null,
+    Edad          int not null,
+    Direccion     varchar(30) not null  
+)
