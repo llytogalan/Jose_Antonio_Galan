be5526997056bd3a85bad6637b62e510f177b337
Ivan <talaveronromeroivan@gmail.com>
2019-10-14 10:21:10 +0200

    crear tabla curso

--- create.sql
+++ create.sql
@@ -5,3 +5,9 @@ create table usuario (
     Edad          int not null,
     Direccion     varchar(30) not null  
 );
+create table cursos (
+    codigo_curso  int Primary key not null,
+    nombre        varchar(30) not null, 
+    Descripcion    varchar(30) not null,
+    Horas_duracion  int not null
+);
